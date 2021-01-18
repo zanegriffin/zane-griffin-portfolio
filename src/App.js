5ce6c822ba3aa62d8a6c700ed48e70a360d8ba02
Zane <zanebgriffin@gmail.com>
2021-01-18 15:13:12 -0600

    initialized react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className='sideblock'></div>
+      <div className='name-block' >
+        <h1>Hi I'm Zane Griffin</h1>
+        <p>I'm a 
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='arrows'>
+
+        </div>
+        <div className='spinner-container'>
+          <div className='spinner'></div>
+          <div className='spinner'></div>
+          <div className='spinner'></div>
+        </div>
+        <div className='link-on-landing'>
+          <a>click here</a>
+        </div>
+      </div>
     </div>
   );
 }
